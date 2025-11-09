f06c37cf5e9113b4ff84eaa521d618cf09041572
Sara Islahi <saragh507@gmail.com>
2025-11-09 01:27:27 +0100

    Add files via upload

---
+++ 01_Python_Basics/Week1/day3/exercise_1.py
@@ -0,0 +1,6 @@
+print(" Exercise 1: Converting Lists Into Dictionaries ")
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [(10,"g",4),(20,"k",6),("m9 n",7,8) ]
+
+combined_dict = dict(zip(keys, values))
+print(combined_dict)
